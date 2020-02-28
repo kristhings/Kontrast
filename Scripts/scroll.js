392e422d8c3624ae703f09598da0a3d0a9f76e43
kristhings <46817772+kristhings@users.noreply.github.com>
2020-02-28 13:50:16 +0100

    upload

---
+++ Scripts/scroll.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("nav").style.height = "80px";
+        document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.75)";
+        document.getElementById("navbar-left").style.height = "30px";
+        document.getElementById("navbar-left").style.width = "30px";
+        document.getElementById("navbar-right").style.marginTop = "-10px";
+      } else {
+        document.getElementById("nav").style.height = "100px";
+        document.getElementById("nav").style.backgroundColor = "white";
+        document.getElementById("navbar-left").style.height = "50px";
+        document.getElementById("navbar-left").style.width = "50px";
+        document.getElementById("navbar-right").style.marginTop = "0";
+      }
+    }
